bcd115e0c85a7c828a4f3c55a0f13a2540f36b64
kenriortega <kenriortega@gmail.com>
2021-04-18 08:29:42 -0400

    [REFORMAT] main code to use koin DI

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,20 @@
 import data.RemoteDataSourceImpl
+import di.Repository
+import di.githubJobModule
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
 import kotlinx.coroutines.*
+import org.koin.core.context.startKoin
 import utils.Constants
 import utils.Timer
 
 suspend fun main() {
+    startKoin {
+        modules(githubJobModule)
+    }
     val client = HttpClient(CIO) {
         install(JsonFeature) {
             serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
@@ -24,7 +30,7 @@ suspend fun main() {
     Timer.setInterval(Constants.FETCH_INTERVAL) {
         println("Doing Fetch data ...")
         CoroutineScope(Dispatchers.IO).launch {
-            val data = async { RemoteDataSourceImpl().getJobsBySearch(client,"kotlin",4) }
+            val data = async { Repository().getJobsBySearch(client,"kotlin",4) }
             val result = data.await()
             println(result.map { it.company })
         }
